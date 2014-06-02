b6fb41674aa4ad3d86e3a71462b209f73e2e97d0
ravenjohn <rjlagrimas08@gmail.com>
2014-06-02 20:36:37 +0800

    Added max handles paramater on crawler

--- crawler/blackwidow7.js
+++ crawler/blackwidow7.js
@@ -33,6 +33,10 @@ var cluster = require('cluster'),
 			this.ecb = cb;
 			return this;
 		};
+		this.finally = function (cb) {
+			this.fcb = cb;
+			return this;
+		};
 		this.send = function (data) {
 			var self = this,
 				protocol,
@@ -75,148 +79,175 @@ var cluster = require('cluster'),
 	};
 
 if (cluster.isMaster) {
-	var cpuCount = require('os').cpus().length,
-		regions = ['US', 'DZ', 'AR', 'AU', 'AT', 'BH', 'BE', 'BA', 'BR', 'BG', 'CA', 'CL', 'CO', 'HR', 'CZ', 'DK', 'EG', 'EE', 'FI', 'FR', 'DE', 'GH', 'GR', 'HK', 'HU', 'IN', 'ID', 'IE', 'IL', 'IT', 'JP', 'JO', 'KE', 'KW', 'LV', 'LB', 'LT', 'MK', 'MY', 'MX', 'ME', 'MA', 'NL', 'NZ', 'NG', 'NO', 'OM', 'PE', 'PH', 'PL', 'PT', 'QA', 'RO', 'RU', 'SA', 'SN', 'RS', 'SG', 'SK', 'SI', 'ZA', 'KR', 'ES', 'SE', 'CH', 'TW', 'TH', 'TN', 'TR', 'UG', 'UA', 'AE', 'GB', 'YE'],
-		split = function (a, n) {
-			var len = a.length,
-				out = [],
-				i = 0,
-				size;
-			while (i < len) {
-				size = Math.ceil((len - i) / n--);
-				out.push(a.slice(i, i += size));
-			}
-			return out;
-		};
-	if (!process.argv[2])
-		return console.log('Server IP is missing');
-	new Request()
-		.to('www.youtube.com', 443, '/channels')
-		.raw()
-		.secured()
-		.send({gl : regions[~~(74 * Math.random())]})
-		.then(function (status, data) {
-			var match;
-			if (status === 200) {
-				match = data.match(/\ytid=\\\"(.{1,50})\"\\/gi);
-				if (match) {
-					split(match
-						.filter(function (e, p, s) {
-							return s.indexOf(e) === p;
-						})
-						.map(function (e) {
-							return e.substring(7, e.length - 3);
-						}), cpuCount)
-						.forEach(function (a) {
-							return cluster.fork({
-								channels : a.join(','),
-								url : process.argv[2]
-							});
-						});
+	(function () {
+		var cpuCount = require('os').cpus().length,
+			region = ['US', 'DZ', 'AR', 'AU', 'AT', 'BH', 'BE', 'BA', 'BR', 'BG', 'CA', 'CL', 'CO', 'HR', 'CZ', 'DK', 'EG', 'EE', 'FI', 'FR', 'DE', 'GH', 'GR', 'HK', 'HU', 'IN', 'ID', 'IE', 'IL', 'IT', 'JP', 'JO', 'KE', 'KW', 'LV', 'LB', 'LT', 'MK', 'MY', 'MX', 'ME', 'MA', 'NL', 'NZ', 'NG', 'NO', 'OM', 'PE', 'PH', 'PL', 'PT', 'QA', 'RO', 'RU', 'SA', 'SN', 'RS', 'SG', 'SK', 'SI', 'ZA', 'KR', 'ES', 'SE', 'CH', 'TW', 'TH', 'TN', 'TR', 'UG', 'UA', 'AE', 'GB', 'YE'][~~(74 * Math.random())],
+			split = function (a, n) {
+				var len = a.length,
+					out = [],
+					i = 0,
+					size;
+				while (i < len) {
+					size = Math.ceil((len - i) / n--);
+					out.push(a.slice(i, i += size));
 				}
-			}
-		})
-		.onerror(console.dir);
-} else {
-	var io = require('socket.io-client'),
-		KEY = 'AIzaSyDqWOahd3OSYfctw5pTTcNjQjjfD3QC-s4',
-		get_scm = function (a) {
-			return new Request()
-				.to('www.youtube.com', 443, '/watch')
-				.raw()
-				.secured()
-				.send({v : a.first_video})
-				.then(function (status, data) {
-					var match;
-					if (status === 200) {
-						match = data.match(/\<meta name=(\")?attribution(\")?(\s*)content=(.{1,50})\>/gi);
-						if (match && match[0]) {
-							match = match[0].substring(31);
-							a.scm = match.substring(0, match.length - 2);
-						}
-						match = data.match(/\ytid=\\\"(.{1,50})\"\\/gi);
-						if (match) {
-							match
-								.filter(function (e, p, s) {
-									return s.indexOf(e) === p;
-								})
-								.forEach(function (e) {
-									return setTimeout(function (e) {
-										return get_channel(e.substring(7, e.length - 3));
-									}, 1000, e);
+				return out;
+			};
+
+		if (!process.argv[2]) {
+			return console.log('Server IP is missing');
+		}
+
+		if (!process.argv[3]) {
+			console.log('2nd paramater : MAX connection per thread is missing. Defaulting to 300');
+			process.argv[3] = 200;
+		}
+		else {
+			process.argv[3] = +process.argv[3];
+		}
+
+		console.log('Server : http://' + process.argv[2]);
+		console.log('Region : ' + region);
+		console.log('CPU count : ' + cpuCount);
+		console.log('MAX handles per thread : ' + process.argv[3]);
+
+		new Request()
+			.to('www.youtube.com', 443, '/channels')
+			.raw()
+			.secured()
+			.send({gl : region})
+			.then(function (status, data) {
+				var match;
+				if (status === 200) {
+					match = data.match(/\ytid=\\\"(.{1,50})\"\\/gi);
+					if (match) {
+						split(match
+							.filter(function (e, p, s) {
+								return s.indexOf(e) === p;
+							})
+							.map(function (e) {
+								return e.substring(7, e.length - 3);
+							}), cpuCount)
+							.forEach(function (a) {
+								return cluster.fork({
+									channels : a.join(','),
+									url : process.argv[2],
+									max : process.argv[3]
 								});
-						}
+							});
 					}
-					if (a.statistics) {
-						delete a.first_video;
-						client.emit('insert', a);
-					}
-					return;
-				})
-				.onerror(function () {
-					if (a.statistics) {
-						delete a.first_video;
-						client.emit('insert', a);
-					}
-				});
-		},
-
-		get_username = function (a) {
-			return new Request()
-				.to('www.googleapis.com', 443, '/youtube/v3/search')
-				.secured()
-				.send({
-					part : 'snippet',
-					channelId : a._id,
-					type : 'video',
-					maxResults : 1,
-					fields : 'items(id/videoId,snippet/channelTitle)',
-					key : KEY
-				})
-				.then(function (status, data) {
-					if (status === 200) {
-						if (data.items[0] && data.items[0].id && !(a.first_video = data.items[0].id.videoId)) return;
-						if (data.items[0] && data.items[0].snippet && !(a.username = data.items[0].snippet.channelTitle)) return;
-						return get_scm(a);
-					} else {
-						console.log(status, 'Error getting first video and username of channel', a._id);
-					}
-					return;
-				})
-				.onerror(console.dir);
-		},
-
-		get_channel = function (a) {
-			return new Request()
-				.to('www.googleapis.com', 443, '/youtube/v3/channels')
-				.secured()
-				.send({
-					part : 'id, statistics',
-					id : a,
-					fields : 'items(statistics)',
-					key : KEY
-				})
-				.then(function (status, data) {
-					if (status === 200) {
-						if (data.items && data.items[0]) {
-							data = data.items[0];
-							if (+data.statistics.videoCount > 0) {
-								data.date_crawled = +new Date;
-								data._id = a;
-								data.scm = null;
-								return get_username(data);
+				}
+			})
+			.onerror(console.dir);
+	}());
+} else {
+	setTimeout(function () {
+		var io = require('socket.io-client'),
+			KEY = 'AIzaSyDqWOahd3OSYfctw5pTTcNjQjjfD3QC-s4',
+			max,
+			request_count = 0,
+			get_scm = function (a) {
+				return new Request()
+					.to('www.youtube.com', 443, '/watch')
+					.raw()
+					.secured()
+					.send({v : a.first_video})
+					.then(function (status, data) {
+						var match;
+						if (status === 200) {
+							match = data.match(/\<meta name=(\")?attribution(\")?(\s*)content=(.{1,50})\>/gi);
+							if (match && match[0]) {
+								match = match[0].substring(31);
+								a.scm = match.substring(0, match.length - 2);
 							}
-						};
-					}
-					else
-						console.log(status, 'Error on getting channel', a);
-					return;
-				})
-				.onerror(console.dir);
-		};
-	console.log('Releasing black widow', cluster.worker.id);
-	client = io.connect('http://' + process.env['url']);
-	process.env['channels'].split(',').forEach(get_channel);
+							match = data.match(/\ytid=\\\"(.{1,50})\"\\/gi);
+							if (match) {
+								if (request_count > max) return;
+								match
+									.forEach(function (e, p, s) {
+										if (s.indexOf(e) === p) {
+											return get_channel(e.substring(7, e.length - 3));
+										}
+									});
+							}
+						}
+						if (a.statistics) {
+							delete a.first_video;
+							client.emit('insert', a);
+						}
+						return;
+					})
+					.onerror(function () {
+						if (a.statistics) {
+							delete a.first_video;
+							client.emit('insert', a);
+						}
+					})
+					.finally(function () {
+						request_count--;
+					});
+			},
+
+			get_username = function (a) {
+				return new Request()
+					.to('www.googleapis.com', 443, '/youtube/v3/search')
+					.secured()
+					.send({
+						part : 'snippet',
+						channelId : a._id,
+						type : 'video',
+						maxResults : 1,
+						fields : 'items(id/videoId,snippet/channelTitle)',
+						key : KEY
+					})
+					.then(function (status, data) {
+						if (status === 200) {
+							if (data.items[0] && data.items[0].id && !(a.first_video = data.items[0].id.videoId)) return;
+							if (data.items[0] && data.items[0].snippet && !(a.username = data.items[0].snippet.channelTitle)) return;
+							return get_scm(a);
+						} else {
+							console.log(status, 'Error getting first video and username of channel', a._id);
+						}
+						return;
+					})
+					.onerror(console.dir);
+			},
+
+			get_channel = function (a) {
+				request_count++;
+				return new Request()
+					.to('www.googleapis.com', 443, '/youtube/v3/channels')
+					.secured()
+					.send({
+						part : 'id, statistics',
+						id : a,
+						fields : 'items(statistics)',
+						key : KEY
+					})
+					.then(function (status, data) {
+						if (status === 200) {
+							if (data.items && data.items[0]) {
+								data = data.items[0];
+								if (+data.statistics.videoCount > 0) {
+									data.date_crawled = +new Date;
+									data._id = a;
+									data.scm = null;
+									return get_username(data);
+								}
+							};
+						}
+						else
+							console.log(status, 'Error on getting channel', a);
+						return;
+					})
+					.onerror(console.dir);
+			};
+
+		console.log('Releasing black widow', cluster.worker.id);
+		client = io.connect('http://' + process.env['url']);
+		max = +process.env['max'];
+		process.env['channels'].split(',').forEach(get_channel);
+	}, 0);
 }
 
 cluster.on('exit', function (worker) {

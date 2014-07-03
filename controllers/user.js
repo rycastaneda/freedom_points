6f42be4e862b4ea528a3b69142d0662a5693ccf1
ravenjohn <rjlagrimas08@gmail.com>
2014-07-03 18:19:01 +0800

    Put google data back on cookies

--- controllers/user.js
+++ controllers/user.js
@@ -58,8 +58,8 @@ exports.auth_google_callback = function (req, res, next) {
                 case 1 :
                     if (~user.email.indexOf('@pages.plusgoogle.com'))
                         user.email = '';
-                    res.setHeader('X-Google-Data', JSON.stringify(user));
-                    res.redirect(config.frontend_server_url + '/auth/register');
+                    res.cookie('data', JSON.stringify(user));
+                    res.redirect(config.frontend_server_url + '/register');
             }
         },
         login_to_AS = function (err, response) {
